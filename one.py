f4fcd0f9227d156ab0e8996678a6f313f4ab9124
afikmirc <afikmirc8@gmail.com>
2019-10-12 10:25:45 +0300

    perfect

--- one.py
+++ one.py
@@ -1,17 +1,5 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
 print ("stammaster")
-=======
 print ("stam3")
 print ("WTF")
->>>>>>> dev
-=======
 print ("222")
-=======
 print ("stam3")
-<<<<<<< HEAD
->>>>>>> parent of a3bd029... Revert "'wtf'"
-=======
->>>>>>> parent of e53e101... best version
->>>>>>> parent of e71f562... Revert "Revert "best version""
->>>>>>> parent of 0b32a2e... new world
