a67a2b421c6a52d0f0d278d4be08bb1d5fa8fc6e
afikmirc <afikmirc8@gmail.com>
2019-10-12 10:00:25 +0300

    Revert "Revert "Revert "best version"""
    This reverts commit e71f56205e884dc9bfd53b8917ea96019f5a2adb.

--- one.py
+++ one.py
@@ -2,4 +2,8 @@
 print ("222")
 =======
 print ("stam3")
+<<<<<<< HEAD
 >>>>>>> parent of a3bd029... Revert "'wtf'"
+=======
+>>>>>>> parent of e53e101... best version
+>>>>>>> parent of e71f562... Revert "Revert "best version""
