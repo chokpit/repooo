0b32a2edd5f97b0b800ba6148c114514e8a3bf73
afikmirc <afikmirc8@gmail.com>
2019-10-12 10:03:17 +0300

    new world

--- one.py
+++ one.py
@@ -1,9 +1,1 @@
-<<<<<<< HEAD
-print ("222")
-=======
 print ("stam3")
-<<<<<<< HEAD
->>>>>>> parent of a3bd029... Revert "'wtf'"
-=======
->>>>>>> parent of e53e101... best version
->>>>>>> parent of e71f562... Revert "Revert "best version""
